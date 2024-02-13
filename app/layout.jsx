659d1c5a53f47beb4a5c4e9a0953da8db311ed90
deepGIVAn <deepakpvt26@gmail.com>
2024-02-13 15:31:36 +0530

    changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,25 +2,25 @@
 import "./globals.css";
 import "./data-tables-css.css";
 import "./satoshi.css";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import Loader from "@/components/common/Loader";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
-import useAuth from "@/hooks/useAuth";
-import { useRouter, usePathname } from "next/navigation";
-import firebase from "firebase/compat/app";
-import firebaseConfig from "@/config/firebaseConfig";
+// import useAuth from "@/hooks/useAuth";
+// import { useRouter, usePathname } from "next/navigation";
+// import firebase from "firebase/compat/app";
+// import firebaseConfig from "@/config/firebaseConfig";
 
 // firebase.initializeApp(firebaseConfig);
 
 export default function RootLayout({ children }) {
-  const navigate = useRouter();
-  const isLoggedIn = useAuth();
-  console.log(isLoggedIn);
-  const pathname = usePathname();
+  // const navigate = useRouter();
+  // const isLoggedIn = useAuth();
+  // console.log(isLoggedIn);
+  // const pathname = usePathname();
 
   // const [loading, setLoading] = useState(true);
   // useEffect(() => {
